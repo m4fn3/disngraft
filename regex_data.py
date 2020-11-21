53b06d7a5503c77324e30717fd4e391ec46dbc40
mafusuke <mafusukee@gmail.com>
2020-11-21 15:49:06 +0900

    Improved log regex

--- regex_data.py
+++ regex_data.py
@@ -5,7 +5,7 @@ import re
 class LogRegex:
     server_start = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: Done \((.*)\)! For help, type \"help\"")
     server_stop = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: Stopping server")
-    player_join = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: ([0-9a-zA-Z_]*)\[.*\] logged in with entity id.*")
-    player_leave = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: ([0-9a-zA-Z_]*) left the game")
-    on_chat = re.compile(r"\[\d+:\d+:\d+\] \[(?:Server thread|Async Chat Thread - #\d+)/INFO\]: <(.*)> (.*)")
-    on_tell = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: You whisper to [0-9a-zA-Z_]*: (.*)")
+    player_join = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: ([0-9a-zA-Z_]{3,16})\[.+\] logged in with entity id.*")
+    player_leave = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: ([0-9a-zA-Z_]{3,16}) left the game")
+    on_chat = re.compile(r"\[\d+:\d+:\d+\] \[(?:Server thread|Async Chat Thread - #\d+)/INFO\]: <[0-9a-zA-Z_]{3,16}> (.*)")
+    on_tell = re.compile(r"\[\d+:\d+:\d+\] \[Server thread/INFO\]: You whisper to [0-9a-zA-Z_]{3,16}: (.*)")
